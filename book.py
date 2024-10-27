e9bccadf829850249acccbc8631bb40cdd038f42
Polina <psapunzhi03@mail.ru>
2024-10-27 18:32:35 +0100

    сделали связь с базой данных

--- book.py
+++ book.py
@@ -1,54 +1,50 @@
 import json 
 import sqlite3
 
-conn = sqlite3.connect("sqlite/Base_Books.db")
+conn = sqlite3.connect("Base_Books.db")
 cursor = conn.cursor()
 
 
 def add_book():
     name = input("name: ")
     author_name = input("author: ")
+    ganre = input("ganre: ")
+    year = input("year: ")
 
     cursor.execute("SELECT author_id FROM authors_table WHERE name = ? ", (author_name,))
     author_row = cursor.fetchone()
     author_id = author_row[0]
 
-    # rating = input("rating: ")
-    # characterictic = input("characterictic: ")
-    
-    # book = {"name": name, "author": author, "rating": rating, "characterictic": characterictic}
-    # list_book.append(book)
 
     cursor.execute("""
-                   INSERT INTO books_table (name, author_id) VALUES (?, ?)
+                   INSERT INTO books_table (name, author_id, ganre, years) VALUES (?, ?, ?,?)
                    """,
-                    (name, author_id))
+                    (name, author_id, ganre, year))
     conn.commit()
+
 
 #выбрать
 def get_book():
-    search_word = input("search: ")
-  
-    for book in list_book:
-        if search_word in book["name"] or search_word in book["author"]:
-            print(book)
-            return book
+    book_name = input("search: ")
+
+    cursor.execute("""SELECT books_table.book_id, authors_table.name, books_table.name FROM authors_table
+                INNER JOIN books_table ON authors_table.author_id = books_table.author_id 
+                WHERE books_table.name = ?
+                   """,
+                   (book_name,))
+    book = cursor.fetchone()
+
+    return book   
 
 def edit_book():
     book = get_book()
-    key = input("edit: ")
-    key in book.keys()
-    if key in book.keys():
-        book[key] = input(key + ' :')
-    else:
-        answer = input("add key?" )
-        if answer == "yes":
-            book[key] = input(key + ' :')
+    print(book)
+    book_id = book[0]
+    name_book = input("new name: ")
 
-def add_keytoallbooks():
-    key = input("edit all: ")
-    for book in list_book:
-        book[key] = input(key + ' :')
+    cursor.execute("UPDATE books_table SET name =? WHERE book_id = ?", (name_book, book_id))
+    conn.commit()
+
         
 
  
@@ -67,7 +63,7 @@ with open('book.json', 'r', encoding='utf-8') as file:
     list_book = json.load(file)
 
 
-message = input("add/get/edit/edit all:  ")
+message = input("add/get/edit:  ")
 if message == "add" :            
     add_book()
 
@@ -77,8 +73,7 @@ if message == "get" :
 if message == "edit" :
     edit_book()
 
-if message == "edit all" :
-    add_keytoallbooks()
+
 
     
 save_books()
